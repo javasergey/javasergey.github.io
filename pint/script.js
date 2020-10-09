f07b7c0db3bd1cfc3e30599f2c13b9fadd612945
javasergey <simracing12@mail.ru>
2020-10-09 15:57:15 +0300

    pint upd

--- pint/script.js
+++ pint/script.js
@@ -27,6 +27,26 @@ let item = [
   {id: 8, name: "./assets/item8.jpg"},
   {id: 9, name: "./assets/item9.jpg"},
   {id: 10, name: "./assets/item10.jpg"},
+  {id: 11, name: "./assets/item11.jpg"},
+  {id: 12, name: "./assets/item12.jpg"},
+  {id: 13, name: "./assets/item13.jpg"},
+  {id: 14, name: "./assets/item14.jpg"},
+  {id: 15, name: "./assets/item15.jpg"},
+  {id: 16, name: "./assets/item16.jpg"},
+  {id: 17, name: "./assets/item17.jpg"},
+  {id: 18, name: "./assets/item18.jpg"},
+  {id: 19, name: "./assets/item19.jpg"},
+  {id: 20, name: "./assets/item20.jpg"},
+  {id: 21, name: "./assets/item21.jpg"},
+  {id: 22, name: "./assets/item22.jpg"},
+  {id: 23, name: "./assets/item23.jpg"},
+  {id: 24, name: "./assets/item24.jpg"},
+  {id: 25, name: "./assets/item25.jpg"},
+  {id: 26, name: "./assets/item26.jpg"},
+  {id: 27, name: "./assets/item27.jpg"},
+  {id: 28, name: "./assets/item28.jpg"},
+  {id: 29, name: "./assets/item29.jpg"},
+  {id: 30, name: "./assets/item30.jpg"}
 ];
 
 new Vue({
